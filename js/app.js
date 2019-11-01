5039af8968fea7d9487307cf53287a51bb946b52
JMoney-216 <jdm216@gmail.com>
2019-11-01 11:06:29 -0500

    created assets folder and added mockup for level 1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+//input for temp
+//input for flow rate
+/*----- functions -----*/
