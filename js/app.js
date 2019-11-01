f16ded77caf76e05fe93c8932e98d2a48ec2da45
JMoney-216 <jdm216@gmail.com>
2019-11-01 11:33:53 -0500

    setup JS file and added function to create and append input vessels

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
 /*----- constants -----*/
+const InputChem1 = {};
 
 /*----- app's state (variables) -----*/
+let createdInputVessels = {};
+
 /*----- cached element references -----*/
+let stage = document.getElementById('stage');
+let inputs = document.getElementById('inputs')
+
 /*----- event listeners -----*/
 //input for temp
 //input for flow rate
 /*----- functions -----*/
+let inputVessel = function(num) {
+    for(i=0; i<num; i++) {
+        createdInputVessels = i[document.createElement("div")];
+        createdInputVessels[i].classList.add('inputVessel')
+    }
+    inputs.appendChild(createdInputVessels)
+}
+
+
+/*---model/data---*/
+
+
+/*---controller---*/
+
+
+/*---ui controller---*/
+//generate level 1
