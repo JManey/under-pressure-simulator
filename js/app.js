c90fe73b655896d5dab6a2d0c53fb6393b50e836
JMoney-216 <jdm216@gmail.com>
2019-11-03 17:55:56 -0600

    changed from dynmic level generation to separate html pages

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 /*----- constants -----*/
 const InputChem1 = {};
+const winQuality = 90;
 
 /*----- app's state (variables) -----*/
 let createdInputVessels = [];
 let currentLevel = 1;
-let newGasRat = null;
+let newGasRate = null;
+let newFluidRate = null;
+let maxGasRate = 100;
+let maxFluidRate = 100;
+let quality = level1Quality();
 
 /*----- cached element references -----*/
 let stage = document.getElementById('stage');
@@ -17,63 +22,31 @@ let burner0 = document.getElementById('burner0');
 // let outPump0 = document.getElementById('outPump0');
 
 /*----- functions -----*/
-function createVessel(num,type){
-    for(i=0; i<num; i++) {
-        let divNew = document.createElement('div');
-        divNew.classList.add(type);
-        let progressNew = document.createElement('progress');
-        progressNew.classList.add("max='100'", 'progressBar' + i)
-        switch(type){
-            case 'inputVessel':
-                stage.append(divNew);
-                progressNew.classList.add('brown')
-                divNew.append(progressNew);
-                let pump = document.createElement('div');
-                pump.classList.add('pump'+i, 'pump');
-                pump.id = 'pump' + i;
-                stage.append(pump);
-                break;
-            case 'processVessel':
-                stage.append(divNew);
-                progressNew.classList.add('red');
-                divNew.append(progressNew);
-                let burner = document.createElement('div');
-                burner.classList.add('burner'+i, 'burner');
-                burner.id = 'burner' + i;
-                stage.append(burner);
-                break;
-            case 'outputVessel':
-                stage.append(divNew);
-                progressNew.classList.add('green');
-                divNew.append(progressNew);
-                let outPump = document.createElement('div');
-                outPump.classList.add('outPump'+i, 'pump');
-                outPump.id = 'outPump' + i;
-                stage.append(outPump);
-                break;
-        }
-    }
-}
 
 function adjustTemp(){
-    let newGasRate = prompt('Enter the gas flow rate, 0% to 100%', [50]);
+    newGasRate = parseInt(prompt('Enter the gas flow rate, 0% to 100%', [50]));
     return newGasRate;
 }
+function adjustRate(){
+    newFluidRate = parseInt(prompt('Enter the fluid flow rate, 0% to 100%', [50]));
+    return newFluidRate;
+}
+
 //negative event 1 gas restriction causes process temp to fall
 //player needs to decrease rate to maintain temp and output quality
 function event1() {
-    let maxTemp = .6;
+    if(newGasRate > 60) {newGasRate = 60;}
+    let maxGasRate = 60;
 }
 /*---model/data---*/
 function level1 () {
-    createVessel(1, 'inputVessel');
-    createVessel(1, 'processVessel');
-    createVessel(1, 'outputVessel');
-    //initiate level paramenters
-    //flow rate(input & output) = gpm, starting temp = degrees F  
-        let initialInput = 80;
-        let initialOutput = 70;
-        let initialTemp = 140;
+    // createVessel(1, 'inputVessel');
+    // createVessel(1, 'processVessel');
+    // createVessel(1, 'outputVessel');
+    // //initiate level paramenters
+    // //flow rate(input & output) = gpm, starting temp = degrees F  
+    //     newFlowRate = 40;
+    //     newGasRate = 44;
     //call negative event function
         //level1 event heating gas shortage
         // after 10 seconds => console.log('Oh no the heating gas pressure dropped, the max heat is limited to 60%.')
@@ -90,12 +63,21 @@ function level2 () {
     createVessel(2, 'processVessel');
     createVessel(1, 'outputVessel');
 }
-
+function level1Quality(){
+    let quality = (newGasRate * .4) + (newFluidRate * .6);
+    return quality;
+}
+function checkQualityForWin() {
+    if(level1Quality() >= winQuality){
+        return alert('game is won');
+    }   else return alert(`Keep trying current quality is ${quality}`);
+}
 
 /*---controller---*/
 function init () {
     level1();
 }
+// setInterval(checkQualityForWin, 1000 * 20);
 
 /*---ui controller---*/
 //generate level 1
@@ -110,7 +92,12 @@ stage.addEventListener('click', function(event){
     if(event.target.id === 'burner0') {
         adjustTemp();
     }
-
 });
 //input for flow rate
-// pump0.addEventListener('click', adjustPump());
+stage.addEventListener('click', function(event){
+    // console.log(event.target.id);
+    if(event.target.id === 'pump0') {
+        adjustRate();
+        // console.log(newFluidRate);
+    }
+});
