4a57a82f18f6d583e6f716b5cd6ddc73c782e2bf
JMoney-216 <jdm216@gmail.com>
2019-11-06 13:19:35 -0600

    make app a single page, level 1 and 2 links are working, level 2 temp 2 still not working

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
 /*----- constants -----*/
 // var Chart = require('chart.js');
-const InputChem1 = {};
+// const InputChem1 = {};
 const winQuality = 95;
-const tempGradient = .48;
+const winQuality2 = 95;
+const tempGradient = .28;
 
 /*----- app's state (variables) -----*/
 let createdInputVessels = [];
 let currentLevel = 1;
-let newGasRate = 50;
+let newGasRate = 30;
 let newGasRate2 = 25;
 let newFluidPercent = 35;
-let fluidRate = newFluidPercent / 100 * 252;
+let fluidRate = newFluidPercent / 100 * 320;
 let maxGasRate = 100;
 let maxFluidRate = 100;
 let quality = null;
-let fluidTemp = tempGradient * (fluidRate / 100 * 252);
+let fluidTemp = tempGradient * (fluidRate / 100 * 320);
 let currentTemp = ((newGasRate / 100 * 400) - ((1 - tempGradient)) * fluidTemp);
-let currentTemp2 = ((newGasRate2 / 100 * 800) - ((1 - tempGradient)) * fluidTemp);
+let currentTemp2 = ((newGasRate2 / 100 * 400) - ((1 - tempGradient)) * fluidTemp)*1.2;
 
 /*----- cached element references -----*/
 let stage = document.getElementById('stage');
@@ -26,58 +27,29 @@ let outputVessel = document.getElementById('output0');
 let controls = document.getElementById('controls');
 let pump0 = document.getElementById('pump0');
 let burner0 = document.getElementById('burner0');
+let burner1 =document.getElementById('burner1');
 let chart1 = document.querySelector('h5');
 let sliderDiv = document.getElementById('sliderDiv');
 
 // let outPump0 = document.getElementById('outPump0');
 
-/*----- event listeners -----*/
-//input for burner0 temp
-window.addEventListener('click', function(event){
-    if(event.target.id === 'level1') {
-        init(1);
-        return;
-        // level1();
-    } else if(event.target.id === 'level2') {
-        init(2);
-        return;
-        // level2();
-    }
-
-})
-stage.addEventListener('click', function(event){
-    // console.log(event.target.id);
-    if(event.target.id === 'burner0') {
-        adjustTemp();
-        updateTemps();
-        checkQuality();
-    }
-});
-//input for flow rate
-stage.addEventListener('click', function(event){
-    // console.log(event.target.id);
-    if(event.target.id === 'pump0') {
-        adjustRate();
-        updateTemps();
-        checkQuality();
-        // console.log(newFluidRate);
-    }
-});
-
-
-
 /*----- functions -----*/
 
 function adjustTemp() {
     newGasRate = parseInt(burner0.value);
-    newGasRate2 = parseInt(burner1.value);
     updateTemps();
     checkQuality();
     return newGasRate;
 }
+function adjustTemp2() {
+    newGasRate2 = parseInt(burner1.value);
+    updateTemps2();
+    checkQualityLvl2();
+    return newGasRate2;
+}
 function adjustRate() {
     newFluidPercent = parseInt(pump0.value);
-    fluidRate = newFluidPercent / 100 * 252;
+    fluidRate = newFluidPercent / 100 * 320;
     updateTemps();
     checkQuality();
     return newFluidPercent;
@@ -94,6 +66,7 @@ function level1 () {
     checkQuality();  
 }
 function level2 () {
+    checkQuality();
     checkQualityLvl2();
     level2Chart();
     level2Slider();
@@ -101,8 +74,19 @@ function level2 () {
 function updateTemps() {
     fluidTemp = tempGradient * (fluidRate / 100 * 252);
     currentTemp = ((newGasRate / 100 * 400) - ((1 - tempGradient)) * fluidTemp);
-    currentTemp2 = ((newGasRate / 100 * 800) - ((1 - tempGradient - .18)) * fluidTemp);
-    return fluidTemp;
+    if(currentTemp <= 0) {
+        currentTemp = 0;
+    }
+}
+function updateTemps2() {
+    fluidTemp2 = tempGradient * (fluidRate / 100 * 252);
+    currentTemp2 = ((newGasRate / 100 * 400) - ((1 - tempGradient)) * fluidTemp);
+        currentTemp2 = ((newGasRate2 / 100 * 400) - ((1 - tempGradient)) * fluidTemp);
+    if(currentTemp2 <= 0) {
+        currentTemp2 = 0;
+    }
+
+    return currentTemp2;
 }
 function checkQuality(){
     if(currentTemp > 140 && currentTemp < 160) {
@@ -114,18 +98,30 @@ function checkQuality(){
     return quality;
 }
 function checkQualityLvl2() {
-    if(currentTemp > 196 && currentTemp < 186) {
-        quality = 100 - Math.abs(currentTemp-191);
+    if(currentTemp > 140 && currentTemp < 160) {
+        quality = 100 - Math.abs(currentTemp-150);
     } else {
-        quality = 100 - 10 - currentTemp * .2;
+        quality = 100 - 10 - currentTemp * .3;
     }
-    checkQualityForWin();
+    if(currentTemp2 > 196 && currentTemp2 < 186) {
+        quality = 100 - Math.abs(currentTemp2-191);
+    } else {
+        quality2 = 100 - 10 - currentTemp2 * .2;
+    }
+
+    checkQualityForWin2();
 }
 function checkQualityForWin() {
     if(quality >= winQuality){
         return alert('game is won');
     }   else return;
 }
+function checkQualityForWin2() {
+    if(quality >= winQuality && quality2 >= winQuality2){
+        return alert('game is won');
+    }   else return;
+}
+
 function level2Slider() {
     let extraSliderDiv = document.createElement('div');
     extraSliderDiv.classList.add('sliders');
@@ -139,13 +135,22 @@ function level2Slider() {
     extraSliderDiv.appendChild(input3);
     sliderDiv.appendChild(extraSliderDiv);
     sliderDiv.appendChild(label);
+
+    stage.addEventListener('click', function(event){
+        // console.log(event.target.id);
+        if(event.target.id === 'burner1') {
+            adjustTemp();
+            updateTemps();
+            checkQuality();
+            checkQualityLvl2();
+        }
+    });
 }
 
 
 //////////////add a dynamic chart
 //start when page loads
 function level1Chart() {
-window.onload = function () {
     var dps = []; // dataPoints
     var dpsPump = []; // pumpDataPoints
     var dpsQuality = []; 
@@ -257,11 +262,9 @@ window.onload = function () {
     setInterval(function(){updateChart()}, updateInterval);
     
     }
-}
 //////////////end chart stuff level 1
 
 function level2Chart() {
-    window.onload = function () {
         var dps = []; // dataPoints
         var dpsBurner2 = [];
         var dpsPump = []; // pumpDataPoints
@@ -394,8 +397,50 @@ function level2Chart() {
         setInterval(function(){updateChart()}, updateInterval);
         
         }
-    }
     //////////////end chart stuff level 2
+
+
+// setInterval(checkQualityForWin, 1000 * 20);
+
+/*---ui controller---*/
+//generate level 1
+//extra slider for level 2
+
+
+/*----- event listeners -----*/
+//input for burner0 temp
+// window.addEventListener('click', function(event){
+//     if(event.target.id === 'level1') {
+//         init(1);
+//         return;
+//         // level1();
+//     } else if(event.target.id === 'level2') {
+//         init(2);
+//         return;
+//         // level2();
+//     }
+
+// })
+stage.addEventListener('click', function(event){
+    // console.log(event.target.id);
+    if(event.target.id === 'burner0') {
+        adjustTemp();
+        updateTemps();
+        checkQuality();
+    }
+});
+
+//input for flow rate
+stage.addEventListener('click', function(event){
+    // console.log(event.target.id);
+    if(event.target.id === 'pump0') {
+        adjustRate();
+        updateTemps();
+        checkQuality();
+        // console.log(newFluidRate);
+    }
+});
+
 
 /*---controller---*/
 function init(num) {
@@ -406,9 +451,16 @@ function init(num) {
     } else return;
     
 }
-// setInterval(checkQualityForWin, 1000 * 20);
 
-/*---ui controller---*/
-//generate level 1
-//extra slider for level 2
+// window.addEventListener('click', function(event){
+//     if(event.target.id === 'level1') {
+//         window.onload(init(1));
+//         return;
+//         // level1();
+//     } else if(event.target.id === 'level2') {
+//         window.onload(init(2));
+//         return;
+//         // level2();
+//     }
 
+// })
