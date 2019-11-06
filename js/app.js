b264f952e26c65cd714ef647caaf601da520dcaa
JMoney-216 <jdm216@gmail.com>
2019-11-06 10:57:57 -0600

    added level 2 graph and controller, just need to get level buttons working onmain page.

--- js/app.js
+++ js/app.js
@@ -7,14 +7,16 @@ const tempGradient = .48;
 /*----- app's state (variables) -----*/
 let createdInputVessels = [];
 let currentLevel = 1;
-let newGasRate = 40;
-let newFluidPercent = 40;
+let newGasRate = 50;
+let newGasRate2 = 25;
+let newFluidPercent = 35;
 let fluidRate = newFluidPercent / 100 * 252;
 let maxGasRate = 100;
 let maxFluidRate = 100;
 let quality = null;
 let fluidTemp = tempGradient * (fluidRate / 100 * 252);
-let currentTemp = ((newGasRate / 100 * 600) - ((1 - tempGradient)) * fluidTemp);
+let currentTemp = ((newGasRate / 100 * 400) - ((1 - tempGradient)) * fluidTemp);
+let currentTemp2 = ((newGasRate2 / 100 * 800) - ((1 - tempGradient)) * fluidTemp);
 
 /*----- cached element references -----*/
 let stage = document.getElementById('stage');
@@ -25,14 +27,23 @@ let controls = document.getElementById('controls');
 let pump0 = document.getElementById('pump0');
 let burner0 = document.getElementById('burner0');
 let chart1 = document.querySelector('h5');
+let sliderDiv = document.getElementById('sliderDiv');
 
 // let outPump0 = document.getElementById('outPump0');
 
 /*----- event listeners -----*/
 //input for burner0 temp
 window.addEventListener('click', function(event){
-    if(event.target.id === 'mainButtons')
-    level1();
+    if(event.target.id === 'level1') {
+        init(1);
+        return;
+        // level1();
+    } else if(event.target.id === 'level2') {
+        init(2);
+        return;
+        // level2();
+    }
+
 })
 stage.addEventListener('click', function(event){
     // console.log(event.target.id);
@@ -54,10 +65,12 @@ stage.addEventListener('click', function(event){
 });
 
 
+
 /*----- functions -----*/
 
 function adjustTemp() {
     newGasRate = parseInt(burner0.value);
+    newGasRate2 = parseInt(burner1.value);
     updateTemps();
     checkQuality();
     return newGasRate;
@@ -75,32 +88,20 @@ function adjustRate() {
 //     let maxGasRate = 60;
 // }
 
-// function navBars() {
-//     let navContainer = document.createElement('div')
-//    let navBar = document.createElement('nav')
-//    let homeButton = document.createElement('a');
-//    homeButton.innerHTML = `<a href="index.html"><button id="mainButtons">Home</button></a>`;
-//    navContainer.appendChild(navBar);
-//    navBar.appendChild(homeButton);
-//    document.body.prepend('navContainer');
-// }
-
-
 /*---model/data---*/
 function level1 () {
     level1Chart();
-    checkQuality();
-   
-   
+    checkQuality();  
 }
 function level2 () {
-    navBars();
     checkQualityLvl2();
     level2Chart();
+    level2Slider();
 }
 function updateTemps() {
     fluidTemp = tempGradient * (fluidRate / 100 * 252);
     currentTemp = ((newGasRate / 100 * 400) - ((1 - tempGradient)) * fluidTemp);
+    currentTemp2 = ((newGasRate / 100 * 800) - ((1 - tempGradient - .18)) * fluidTemp);
     return fluidTemp;
 }
 function checkQuality(){
@@ -112,11 +113,34 @@ function checkQuality(){
     checkQualityForWin();
     return quality;
 }
+function checkQualityLvl2() {
+    if(currentTemp > 196 && currentTemp < 186) {
+        quality = 100 - Math.abs(currentTemp-191);
+    } else {
+        quality = 100 - 10 - currentTemp * .2;
+    }
+    checkQualityForWin();
+}
 function checkQualityForWin() {
     if(quality >= winQuality){
         return alert('game is won');
     }   else return;
 }
+function level2Slider() {
+    let extraSliderDiv = document.createElement('div');
+    extraSliderDiv.classList.add('sliders');
+    let input3 = document.createElement('input');
+    input3.type = "range";
+    input3.classList.add('burner');
+    input3.id = 'burner1';
+    let label = document.createElement('label');
+    label.textContent = "Temperature Adjust 2";
+    label.for = input3;
+    extraSliderDiv.appendChild(input3);
+    sliderDiv.appendChild(extraSliderDiv);
+    sliderDiv.appendChild(label);
+}
+
 
 //////////////add a dynamic chart
 //start when page loads
@@ -180,16 +204,6 @@ window.onload = function () {
             minimum: 0,
             maximum: 100,
         }], 
-        // data: [      
-        //  {
-        //     type: "line",
-        //     axisYType: 'secondary',
-        //     lineColor: "green",
-        //     markerColor: "green",
-        //     dataPoints: dpsQuality,
-        //  }],
-        
-    
     });
     
     var xVal = 0;
@@ -244,7 +258,144 @@ window.onload = function () {
     
     }
 }
-//////////////end chart stuff
+//////////////end chart stuff level 1
+
+function level2Chart() {
+    window.onload = function () {
+        var dps = []; // dataPoints
+        var dpsBurner2 = [];
+        var dpsPump = []; // pumpDataPoints
+        var dpsQuality = []; 
+        var chart = new CanvasJS.Chart("chartContainer", {
+            title :{
+                text: "Current Production"
+            },
+            backgroundColor: 'rgba(111,106,106,.2)',
+            
+            axisY: [
+                {
+                title: 'Temperature',
+                titleFontColor: 'red',
+                labelFontColor: 'red',
+                includeZero: true,
+                minimum: 0,
+                maximum: 500,
+                },
+                {
+                title: 'Rate in GPM',
+                labelFontColor: 'blue',
+                titleFontColor: 'blue',
+                includeZero: true,
+                minimum: 0,
+                maximum: 300,
+                },
+            ],
+            data: [
+                {
+                type: "line",
+                axisYIndex: 0,
+                lineColor: "red",
+                markerColor: "red",
+                dataPoints: dps,
+                },
+                {
+                    type: "line",
+                    axisYIndex: 0,
+                    lineColor: "red",
+                    markerColor: "orange",
+                    dataPoints: dpsBurner2,
+                    },
+                {
+                type: "line",
+                axisYIndex: 1,
+                lineColor: "blue",
+                markerColor: "blue",
+                dataPoints: dpsPump,
+                },
+                {
+                        type: "line",
+                        axisYType: 'secondary',
+                        lineColor: "green",
+                        markerColor: "green",
+                        dataPoints: dpsQuality,
+                     },
+            ], 
+            axisY2: [{
+                title: 'Quality',
+                labelFontColor: 'green',
+                titleFontColor: 'green',
+                includeZero: true,
+                minimum: 0,
+                maximum: 100,
+            }], 
+           
+        });
+        
+        var xVal = 0;
+        var yVal = 100; 
+        var updateInterval = 2000;
+        var dataLength = 20; // number of dataPoints visible at any point
+        
+        var updateChart = function (count) {
+        
+            count = count || 1;
+        
+            for (var j = 0; j < count; j++) {
+                yVal = currentTemp;
+                dps.push({
+                    x: xVal,
+                    y: yVal
+                });
+                xVal++;
+            }
+        
+            if (dps.length > dataLength) {
+                dps.shift();
+            }
+            for (var j = 0; j < count; j++) {
+                yVal = currentTemp2;
+                dpsBurner2.push({
+                    x: xVal,
+                    y: yVal
+                });
+                xVal++;
+            }
+        
+            if (dpsBurner2.length > dataLength) {
+                dpsBurner2.shift();
+            }
+            for (var j = 0; j < count; j++) {
+                yVal = fluidRate;
+                dpsPump.push({
+                    x: xVal,
+                    y: yVal
+                });
+                xVal++;
+            }
+            if (dpsPump.length > dataLength) {
+                dpsPump.shift();
+            }
+            for (var j = 0; j < count; j++) {
+                yVal = quality;
+                dpsQuality.push({
+                    x: xVal,
+                    y: yVal
+                });
+                xVal++;
+            }
+            if (dpsQuality.length > dataLength) {
+                dpsQuality.shift();
+            }
+        
+            chart.render();
+        };
+        
+        updateChart(dataLength);
+        setInterval(function(){updateChart()}, updateInterval);
+        
+        }
+    }
+    //////////////end chart stuff level 2
 
 /*---controller---*/
 function init(num) {
@@ -259,7 +410,5 @@ function init(num) {
 
 /*---ui controller---*/
 //generate level 1
+//extra slider for level 2
 
-init();
-level1();
-level1Chart();
